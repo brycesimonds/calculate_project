f2213f447e14973d3f12a00b999364fbb0654a66
Bryce Simonds <bsimonds14@gmail.com>
2023-01-05 14:26:58 -0700

    Feat: Day attributes are accessible

--- lib/day.rb
+++ lib/day.rb
@@ -1,4 +1,9 @@
 class Day
+  attr_reader :date, :month, :day, :year
   def initialize(date)
+    @date = date 
+    @month = date.split("/")[0]
+    @day = date.split("/")[1]
+    @year = date.split("/")[2]
   end
 end
