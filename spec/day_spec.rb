8bb370aad1356d80c8fec2548b4880b422712d22
Bryce Simonds <bsimonds14@gmail.com>
2023-01-05 14:26:47 -0700

    Test: has attributes

--- spec/day_spec.rb
+++ spec/day_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Day do
     day = Day.new("9/1/15") 
     expect(day).to be_a(Day)
   end
+
+  it "has attributes of month day and year" do 
+    day = Day.new("9/1/15") 
+    expect(day.date).to eq("9/1/15")
+    expect(day.month).to eq("9")
+    expect(day.day).to eq("1")
+    expect(day.year).to eq("15")
+  end
 end
