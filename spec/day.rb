57ecbc0f09bfc016cfb80cfcab6da1931f365b0f
Bryce Simonds <bsimonds14@gmail.com>
2023-01-05 14:12:48 -0700

    Require RSpec and written first Day test in day_spec file

--- spec/day.rb
+++ spec/day.rb
@@ -1,1 +1,9 @@
 require './lib/day'
+require 'rspec'
+
+RSpec.describe Day do 
+  it "exists" do 
+    day = Day.new 
+    expect(day).to be_a(Day)
+  end
+end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,1 +1,2 @@
 require './lib/project'
+require 'rspec'
