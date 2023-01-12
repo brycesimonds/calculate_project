a221a70a9636a74cb619eb761b716c18a2582703
Bryce Simonds <bsimonds14@gmail.com>
2023-01-12 12:03:12 -0700

    All tests passing

--- lib/day.rb
+++ lib/day.rb
@@ -1,10 +1,12 @@
+require 'date'
+require 'pry'
 class Day
   attr_reader :date, :month, :day, :year, :is_travel_day, :is_full_day, :city_cost_type
   def initialize(date, city_cost_type)
-    @date = date 
-    @month = date.split("/")[0]
-    @day = date.split("/")[1]
-    @year = date.split("/")[2]
+    @date = DateTime.parse(date) 
+    @month = date.split("-")[1]
+    @day = date.split("-")[2]
+    @year = date.split("-")[0]
     @is_travel_day = nil
     @is_full_day = nil 
     @city_cost_type = city_cost_type
