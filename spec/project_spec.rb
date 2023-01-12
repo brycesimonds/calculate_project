aabe1d2a2ed7ee5c3c05a913929c19c5a06d207b
Bryce Simonds <bsimonds14@gmail.com>
2023-01-12 10:55:51 -0700

    Test: project exists

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,2 +1,11 @@
 require './lib/project'
 require 'rspec'
+
+RSpec.describe Project do 
+  it "exists" do 
+    start_date = "9/1/15"
+    end_date = "9/3/15"
+    project = Project.new("Low Cost City", start_date, end_date )
+    expect(project).to be_a(Project)
+  end
+end
